503ac1d00d3b30c898947c313420e1695255a498
Fabian Hough <32251754+fabianhough@users.noreply.github.com>
2018-03-06 18:31:58 -0500

    Starting Code in C++ using VS 2017
    Cryptarithmic Problem Solving Program; initial code in C++ to solve initial problem TWO + TWO = FOUR

---
+++ Cryptarithmetic/cryptarithmetic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> letterT = { 5, 6, 7, 8, 9 };
+//vector <int> letter
+
+
+int main() {
+
+
+
+}
+
+
+//index = 0;
+
+void backtrackSearch(int* ans, int& size, int index) {
+	if (size == 5)
+		return;
+
+	//for (int i = 0)
+
+
+}
+
+
+
+
